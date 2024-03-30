17ee55e6622e8c84b48132c1366878a64d45b8c1
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-30 03:09:47 +0100

    advanced task

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,13 @@ typedef struct levelorder_queue_s
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 
+unsigned char is_leaf(const binary_tree_t *node);
+size_t depth(const binary_tree_t *tree);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree, size_t leaf_depth,
+			 size_t level);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
